28c16cf882b8cd701fa07037a8d3bdb2ac110c44
Destiny <772375012@qq.com>
2018-05-24 17:32:57 +0800

    2018-5-24

---
+++ app/src/main/java/com/cykj/survey/fragment/area/ProduceFragment.java
@@ -0,0 +1,15 @@
+package com.cykj.survey.fragment.area;
+
+import android.view.View;
+
+import com.cykj.survey.base.BaseFragment;
+
+public class ProduceFragment extends BaseFragment{
+
+    @Override
+    protected View onCreateView() {
+        return null;
+    }
+
+
+}
---
+++ app/src/main/java/com/cykj/survey/model/Industry.java
@@ -0,0 +1,18 @@
+package com.cykj.survey.model;
+
+import com.cykj.survey.bean.IndustryBean;
+
+import java.util.List;
+
+public class Industry {
+
+    private List<IndustryBean> bean;
+
+    public List<IndustryBean> getBean() {
+        return bean;
+    }
+
+    public void setBean(List<IndustryBean> bean) {
+        this.bean = bean;
+    }
+}
